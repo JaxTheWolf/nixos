b4a4be4c0f3c1457b8598a9ddaf68a1dbec645aa
Roman Lubij <roman.lubij@gmail.com>
2025-11-09 00:43:09 +0100

    Move flatpak defs to programs

--- modules/programs.nix
+++ modules/programs.nix
@@ -32,7 +32,7 @@
             cursor-theme = "Bibata-Modern-Classic";
             clock-format = "24h";
           };
-          
+
           "org/gnome/shell/portal" = {
             color-scheme = "prefer-dark";
           };
@@ -50,7 +50,7 @@
       enable = true;
       lfs.enable = true;
     };
-    
+
     nh = {
       clean = {
         enable = true;
@@ -82,5 +82,45 @@
       remotePlay.openFirewall = true;
     };
     virt-manager.enable = true;
+  };
+
+  services.flatpak = {
+    enable = true;
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }
+    ];
+    packages = [
+      {
+        appId = "com.mattjakeman.ExtensionManager";
+        origin = "flathub";
+      }
+      {
+        appId = "com.prusa3d.PrusaSlicer";
+        origin = "flathub";
+      }
+      {
+        appId = "com.github.tchx84.Flatseal";
+        origin = "flathub";
+      }
+      {
+        appId = "org.gtk.Gtk3theme.Adwaita-dark";
+        origin = "flathub";
+      }
+      {
+        appId = "org.gtk.Gtk3theme.Flat-Remix-GTK-Red-Darkest";
+        origin = "flathub";
+      }
+      {
+        appId = "com.github.iwalton3.jellyfin-media-player";
+        origin = "flathub";
+      }
+      {
+        appId = "org.freecad.FreeCAD";
+        origin = "flathub";
+      }
+    ];
   };
 }
--- modules/services.nix
+++ modules/services.nix
@@ -13,46 +13,6 @@
   services = {
     lact.enable = true;
 
-    flatpak = {
-      enable = true;
-      remotes = [
-        {
-          name = "flathub";
-          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        }
-      ];
-      packages = [
-        {
-          appId = "com.mattjakeman.ExtensionManager";
-          origin = "flathub";
-        }
-        {
-          appId = "com.prusa3d.PrusaSlicer";
-          origin = "flathub";
-        }
-        {
-          appId = "com.github.tchx84.Flatseal";
-          origin = "flathub";
-        }
-        {
-          appId = "org.gtk.Gtk3theme.Adwaita-dark";
-          origin = "flathub";
-        }
-        {
-          appId = "org.gtk.Gtk3theme.Flat-Remix-GTK-Red-Darkest";
-          origin = "flathub";
-        }
-        {
-          appId = "com.github.iwalton3.jellyfin-media-player";
-          origin = "flathub";
-        }
-        {
-          appId = "org.freecad.FreeCAD";
-          origin = "flathub";
-        }
-      ];
-    };
-
     sshd.enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
