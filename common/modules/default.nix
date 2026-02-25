5bcaf7217afe646bdaba596020283ffcd9f823ea
Roman Lubij <roman.lubij@gmail.com>
2026-02-26 00:55:05 +0100

    Commonize attic

--- common/modules/default.nix
+++ common/modules/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./attic.nix
     ./desktop-files.nix
     ./flatpak.nix
     ./gnome.nix
--- laptop/modules/default.nix
+++ laptop/modules/default.nix
@@ -2,7 +2,6 @@
 
 {
   imports = [
-    ./attic.nix
     ./flatpak.nix
     ./gnome.nix
     ./packages.nix
