8c5404e9a4c312d45aa91638e7a9c203e884dd4b
Roman Lubij <roman.lubij@gmail.com>
2026-06-17 20:30:04 +0200

    programs.nix -> default.nix

--- common/modules/home/programs/default.nix
+++ common/modules/home/programs/default.nix
@@ -1,10 +1,78 @@
-{...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   imports = [
     ./bottom.nix
     ./helix.nix
-    ./programs.nix
     ./starship.nix
     ./zellij.nix
     ./zsh.nix
   ];
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    firefox = {
+      enable = true;
+      configPath = "${config.xdg.configHome}/mozilla/firefox";
+      languagePacks = [
+        "cs"
+        "en-GB"
+        "en-US"
+      ];
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
+
+    nh = {
+      clean = {
+        enable = true;
+        extraArgs = "--keep-since 7d --keep 10";
+      };
+      enable = true;
+      flake = "${config.xdg.configHome}/nixos";
+    };
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscode.fhsWithPackages (
+        ps:
+          with ps; [
+            python3
+            zlib
+            gcc
+            gnumake
+          ]
+      );
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    bat.enable = true;
+    eza.enable = true;
+    fd.enable = true;
+    htop.enable = true;
+    jq.enable = true;
+    lazygit.enable = true;
+    ptyxis.enable = true;
+    ripgrep.enable = true;
+    thunderbird.enable = true;
+    zsh.enable = true;
+  };
 }
