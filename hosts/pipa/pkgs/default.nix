436788ad6e2ee3bb952c3afecdf97b66ef5b56f9
Roman Lubij <roman.lubij@gmail.com>
2026-07-03 23:38:47 +0200

    add qbootctl-pipa to overlays

--- hosts/pipa/pkgs/default.nix
+++ hosts/pipa/pkgs/default.nix
@@ -2,4 +2,5 @@ final: prev: {
   pipa-firmware = final.callPackage ./firmware.nix {};
   pipa-kernel = final.callPackage ./kernel.nix {};
   pipa-headers = final.callPackage ./headers.nix {};
+  qbootctl-pipa = final.callPackage ./qbootctl.nix {};
 }
