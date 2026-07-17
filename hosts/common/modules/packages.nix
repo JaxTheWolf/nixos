96fc390075d548d88a39f574dfa67512fb6f9ecc
Roman Lubij <roman.lubij@gmail.com>
2026-07-17 21:33:14 +0200

    gst-vaapi has been deprecated in favour or gst-plugins-* _or something_

--- hosts/common/modules/packages.nix
+++ hosts/common/modules/packages.nix
@@ -62,7 +62,6 @@ in {
       gst_all_1.gst-plugins-base
       gst_all_1.gst-plugins-good
       gst_all_1.gst-plugins-ugly
-      gst_all_1.gst-vaapi
       gst_all_1.gstreamer
       gvfs
       gzip
