1ec04515181841a86eedf3cd669cba36e6603c71
Roman Lubij <roman.lubij@gmail.com>
2026-07-20 17:35:03 +0200

    pipa: Adjust qbootctl pkg to list pipa-headers as a hard dep

--- hosts/pipa/pkgs/qbootctl.nix
+++ hosts/pipa/pkgs/qbootctl.nix
@@ -5,5 +5,7 @@
 qbootctl.overrideAttrs (old: {
   pname = "qbootctl-pipa";
 
+  buildInputs = (old.buildInputs or []) ++ [pipa-headers];
+
   NIX_CFLAGS_COMPILE = "-isystem ${pipa-headers}/include " + (old.NIX_CFLAGS_COMPILE or "");
 })
