3d352d2b93cb2e05a89b9ab8e0659989f0076612
Roman Lubij <roman.lubij@gmail.com>
2026-08-02 12:17:27 +0200

    theming(dalaptop): add Ptyxis UUID

--- hosts/common/modules/home/theming.nix
+++ hosts/common/modules/home/theming.nix
@@ -21,7 +21,7 @@ in {
         gnome.enable = isDesktop;
         qt.enable = isDesktop;
         firefox.enable = false;
-        ptyxis.profileUUIDs = ["57ff3f7eaa21025dead424d66a6cc57a" "110e7a2cb6318f5b41dd40966a3500ed"];
+        ptyxis.profileUUIDs = ["57ff3f7eaa21025dead424d66a6cc57a" "110e7a2cb6318f5b41dd40966a3500ed" "9c09b769039b5190dfdda7766a38688b"];
         vesktop.enable = isDesktop;
         bat.enable = true;
         btop.enable = true;
