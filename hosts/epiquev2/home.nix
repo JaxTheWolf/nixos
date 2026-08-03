7dbd8095ff901f9cf39599080505d49bf2f5f061
Roman Lubij <roman.lubij@gmail.com>
2026-08-03 17:25:45 +0200

    fixup: use xdg.configFile to setup MangoHud programs.MangoHud.settings = { ... } orders the tags alphabetically. This is not desired.

--- hosts/epiquev2/home.nix
+++ hosts/epiquev2/home.nix
@@ -4,60 +4,6 @@
       settings.cpu_sensor = "zenmonitor/Tdie";
     };
 
-    mangohud = {
-      settings = {
-        toggle_fps_limit = "F1";
-        legacy_layout = false;
-        pci_dev = "0:2d:00.0";
-        cpu_stats = true;
-        graphs = "cpu_load,gpu_load";
-        cpu_temp = true;
-        cpu_power = true;
-        cpu_mhz = true;
-        cpu_load_change = true;
-        cpu_color = "DE0835";
-        cpu_text = "R7 5800X";
-        gpu_stats = true;
-        gpu_temp = true;
-        gpu_core_clock = true;
-        gpu_mem_clock = true;
-        gpu_power = true;
-        gpu_text = "GPU";
-        gpu_name = true;
-        io_read = true;
-        io_write = true;
-        io_color = "D8D8D8";
-        swap = true;
-        vram = true;
-        vram_color = "DE0835";
-        ram = true;
-        ram_color = "DE0835";
-        fps = true;
-        engine_version = true;
-        engine_color = "950524";
-        gpu_color = "DE0835";
-        vulkan_driver = true;
-        vkbasalt = true;
-        gpu_fan = true;
-        wine_color = "950524";
-        frame_timing = 1;
-        frametime_color = "DE0835";
-        throttling_status = true;
-        media_player_color = "950524";
-        table_columns = 3;
-        background_alpha = "0.4";
-        font_size = 19;
-        background_color = "020202";
-        position = "top-left";
-        text_color = "D8D8D8";
-        round_corners = 4;
-        toggle_hud = "Shift_R+F12";
-        toggle_logging = "Shift_L+F2";
-        upload_log = "F5";
-        output_folder = "/home/jax";
-      };
-    };
-
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
@@ -72,6 +18,60 @@
   };
 
   xdg.configFile = {
+    "MangoHud/MangoHud.conf".text = ''
+      toggle_fps_limit=F1
+      legacy_layout=false
+      pci_dev=0:2d:00.0
+      cpu_stats
+      graphs=cpu_load
+      cpu_temp
+      cpu_power
+      cpu_mhz
+      cpu_load_change
+      cpu_color=DE0835
+      cpu_text=R7 5800X
+      gpu_stats
+      graphs=gpu_load
+      gpu_temp
+      gpu_core_clock
+      gpu_mem_clock
+      gpu_power
+      gpu_text=GPU
+      gpu_name
+      io_read
+      io_write
+      io_color=D8D8D8
+      swap
+      vram
+      vram_color=DE0835
+      ram
+      ram_color=DE0835
+      fps
+      engine_version
+      engine_color=950524
+      gpu_color=DE0835
+      vulkan_driver
+      vkbasalt
+      gpu_fan
+      wine_color=950524
+      frame_timing=1
+      frametime_color=DE0835
+      throttling_status
+      media_player_color=950524
+      table_columns=3
+      background_alpha=0.4
+      font_size=19
+      background_color=020202
+      position=top-left
+      text_color=D8D8D8
+      round_corners=4
+      toggle_hud=Shift_R+F12
+      toggle_logging=Shift_L+F2
+      upload_log=F5
+      output_folder=/home/jax
+      media_player_name=spotify
+    '';
+
     "autostart/01-zenmonitor.desktop".source = "${pkgs.zenmonitor}/share/applications/zenmonitor.desktop";
 
     "autostart/01-openrgb.desktop".text = ''
