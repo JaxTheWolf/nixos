c63c8eff5c6613fa6063e9f735acf78a720c8d06
Roman Lubij <roman.lubij@gmail.com>
2026-08-03 13:23:57 +0200

    theming: disable global MangoHud theming Add a config override to epiquev2

--- hosts/common/modules/home/theming.nix
+++ hosts/common/modules/home/theming.nix
@@ -18,15 +18,17 @@ in {
         light = "Papirus-Light";
       };
       targets = {
-        gnome.enable = isDesktop;
-        qt.enable = isDesktop;
-        firefox.enable = false;
-        ptyxis.profileUUIDs = ["57ff3f7eaa21025dead424d66a6cc57a" "110e7a2cb6318f5b41dd40966a3500ed" "9c09b769039b5190dfdda7766a38688b"];
-        vesktop.enable = isDesktop;
         bat.enable = true;
         btop.enable = true;
+        gnome.enable = isDesktop;
         helix.enable = true;
+        ptyxis.profileUUIDs = ["57ff3f7eaa21025dead424d66a6cc57a" "110e7a2cb6318f5b41dd40966a3500ed" "9c09b769039b5190dfdda7766a38688b"];
+        qt.enable = isDesktop;
+        vesktop.enable = isDesktop;
         zellij.enable = true;
+
+        firefox.enable = false;
+        mangohud.enable = false;
       };
     };
 
--- hosts/epiquev2/home.nix
+++ hosts/epiquev2/home.nix
@@ -1,14 +1,70 @@
 {pkgs, ...}: {
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      obs-backgroundremoval
-      obs-gstreamer
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-vkcapture
-      wlrobs
-    ];
+  programs = {
+    mangohud = {
+      settings = {
+        toggle_fps_limit = "F1";
+        legacy_layout = false;
+        pci_dev = "0:2d:00.0";
+        cpu_stats = true;
+        graphs = "cpu_load,gpu_load";
+        cpu_temp = true;
+        cpu_power = true;
+        cpu_mhz = true;
+        cpu_load_change = true;
+        cpu_color = "DE0835";
+        cpu_text = "R7 5800X";
+        gpu_stats = true;
+        gpu_temp = true;
+        gpu_core_clock = true;
+        gpu_mem_clock = true;
+        gpu_power = true;
+        gpu_text = "GPU";
+        gpu_name = true;
+        io_read = true;
+        io_write = true;
+        io_color = "D8D8D8";
+        swap = true;
+        vram = true;
+        vram_color = "DE0835";
+        ram = true;
+        ram_color = "DE0835";
+        fps = true;
+        engine_version = true;
+        engine_color = "950524";
+        gpu_color = "DE0835";
+        vulkan_driver = true;
+        vkbasalt = true;
+        gpu_fan = true;
+        wine_color = "950524";
+        frame_timing = 1;
+        frametime_color = "DE0835";
+        throttling_status = true;
+        media_player_color = "950524";
+        table_columns = 3;
+        background_alpha = "0.4";
+        font_size = 19;
+        background_color = "020202";
+        position = "top-left";
+        text_color = "D8D8D8";
+        round_corners = 4;
+        toggle_hud = "Shift_R+F12";
+        toggle_logging = "Shift_L+F2";
+        upload_log = "F5";
+        output_folder = "/home/jax";
+      };
+    };
+
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-backgroundremoval
+        obs-gstreamer
+        obs-pipewire-audio-capture
+        obs-vaapi
+        obs-vkcapture
+        wlrobs
+      ];
+    };
   };
 
   xdg.configFile = {
