37c6a8bcc7c4a530122cc36462997e1e2e1b9a8a
Roman Lubij <roman.lubij@gmail.com>
2026-08-09 23:02:59 +0200

    pipa: mount boot and root based on partlabel

--- hosts/pipa/hardware-configuration.nix
+++ hosts/pipa/hardware-configuration.nix
@@ -1,36 +1,32 @@
-{
-  modulesPath,
-  ...
-}: {
+{modulesPath, ...}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   fileSystems = {
     "/boot" = {
-      device = "/dev/disk/by-label/NIX_BOOT";
+      device = "/dev/disk/by-partlabel/linux_boot";
       fsType = "vfat";
       options = ["fmask=0022" "dmask=0022"];
     };
 
     "/" = {
-      device = "/dev/disk/by-label/NIX_ROOT";
+      device = "/dev/disk/by-partlabel/linux_root";
       fsType = "btrfs";
       options = ["subvol=root" "compress=zstd" "noatime"];
     };
 
     "/nix" = {
-      device = "/dev/disk/by-label/NIX_ROOT";
+      device = "/dev/disk/by-partlabel/linux_root";
       fsType = "btrfs";
       options = ["subvol=nix" "compress=zstd" "noatime"];
       neededForBoot = true;
     };
 
     "/home" = {
-      device = "/dev/disk/by-label/NIX_ROOT";
+      device = "/dev/disk/by-partlabel/linux_root";
       fsType = "btrfs";
       options = ["subvol=home" "compress=zstd" "noatime"];
     };
   };
 }
-
